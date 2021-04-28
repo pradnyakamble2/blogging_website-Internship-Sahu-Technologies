4106da05e67b8d770c4099a2f64bc04a8fc9e85f
saikumar110 <sai999694@gmail.com>
2021-04-28 11:48:16 +0530

    first commit

--- Blogging_site/settings.py
+++ Blogging_site/settings.py
@@ -120,8 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL	=	'/media/'
-MEDIA_ROOT	=	os.path.join(BASE_DIR,	'media/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Sending	emails	with	Django
